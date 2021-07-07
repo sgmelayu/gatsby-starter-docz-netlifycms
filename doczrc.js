031175fccfeabf2f7c84efa34227d3e63a3f9ac9
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-08 04:25:32 +0800

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -3,7 +3,7 @@ const projectRoot = __dirname.replace('/.docz', '');
 const { menuItems } = require(`${projectRoot}/config/menu.json`);
 
 export default {
-  files: 'docs/**/*.{html,md,markdown,mdx}',
+  files: '/docs/**/*.{html,md,markdown,mdx}',
   title: 'Legal Policies For SGMelayu Online',
   menu: menuItems
 }
